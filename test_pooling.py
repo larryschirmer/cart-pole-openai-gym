688987a7696620bcb1b2e68a707a627e1c1665a5
Larry Schirmer <larryschirmer@gmail.com>
2019-07-20 21:03:31 -0400

    control threading manually

--- test_pooling.py
+++ test_pooling.py
@@ -5,16 +5,34 @@ import numpy as np
 def get_slice(list_name, step, i): return list_name[step*i:step*i+step]
 
 
-def square(x):
-    return np.square(x)
+def square(i, x, queue):
+    print("In process {}".format(i,))
+    queue.put(np.square(x))
 
 
 thread_count = mp.cpu_count()
 pool = mp.Pool(thread_count)
-
+processes = []
+queue = mp.Queue()
 x = np.arange(thread_count**2)
+
 print("x: {}".format(x))
 
-number_to_process = [get_slice(x, thread_count, i) for i in range(thread_count)]
-squared = pool.map(square, number_to_process)
-print(squared)
+for thread_index in range(thread_count):
+    proc = mp.Process(target=square, args=(
+        thread_index, get_slice(x, thread_count, thread_index), queue))
+
+    proc.start()
+    processes.append(proc)
+
+for proc in processes:
+    proc.join()
+
+for proc in processes:
+    proc.terminate()
+
+squared_results = []
+while not queue.empty():
+    squared_results.append(queue.get())
+
+print(squared_results)
